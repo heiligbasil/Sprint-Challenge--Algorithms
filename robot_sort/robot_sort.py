33cb919664e52512c5f95a1eb65b8084c9ddbf59
heiligbasil <basilief@yahoo.com>
2020-02-21 10:50:18 -0800

    Implementation of the Robot Sort

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -81,11 +81,13 @@ class SortingRobot:
         Turn on the robot's light
         """
         self._light = "ON"
+
     def set_light_off(self):
         """
         Turn off the robot's light
         """
         self._light = "OFF"
+
     def light_is_on(self):
         """
         Returns True if the robot's light is on and False otherwise.
@@ -96,8 +98,37 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        if self.compare_item() is None:
+            self.swap_item()
+            self.move_right()
+        for i in range(200):
+            self.move_largest_to_right()
+            self.move_smallest_to_left()
+        self.pick_none_back_up()
+
+    def move_largest_to_right(self):
+        while True:
+            if self.compare_item() < 0:
+                self.swap_item()
+            if self.can_move_right():
+                self.move_right()
+            else:
+                break
+
+    def move_smallest_to_left(self):
+        while True:
+            if self.compare_item() is None:
+                self.move_right()
+                break
+            if self.compare_item() > 0:
+                self.swap_item()
+            if self.can_move_left():
+                self.move_left()
+
+    def pick_none_back_up(self):
+        while self.can_move_left():
+            self.move_left()
+        self.swap_item()
 
 
 if __name__ == "__main__":
